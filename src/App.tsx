dd20a043119dfbfcf72ce17f5a7f027ad0c7c9c6
guimkogus <guilherme.mkogus@gmail.com>
2022-09-18 14:54:57 -0300

    chore: set up eslint, prettier and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { handleHelloWorldAlert } from "./utils";
+import React from 'react';
+import { handleHelloWorldAlert } from './utils';
 
 const App = () => {
   return (
     <div>
       <div>Hello PokeFederations (ui-utils)</div>
-      <button onClick={handleHelloWorldAlert}>
-          exec handleHelloWorldAlert
-      </button>
+      <button onClick={handleHelloWorldAlert}>exec handleHelloWorldAlert</button>
     </div>
   );
 };
